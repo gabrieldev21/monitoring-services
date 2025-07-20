6214f4d99d58abfb35b9a33467e31a8d7f33f3ca
gabrieldev21 <gabrielp.amorim@live.com>
2025-07-20 10:40:46 -0300

    fix: ajuste ms-auth

--- apps/ms-auth/src/main.ts
+++ apps/ms-auth/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { MsAuthModule } from './ms-auth.module';
+import { AppModule } from './modules/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MsAuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: 3001,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
