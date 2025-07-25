322d794df4a15686a48f0149913388bf479a4f80
Gabriel Amorim <gabriel.amorim@profarma.com.br>
2025-07-25 16:30:14 -0300

    chore: import app module tcc

--- apps/ms-auth/src/modules/app.module.ts
+++ apps/ms-auth/src/modules/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MetricModule } from './metric/metric.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MetricModule],
 })
 export class AppModule {}
