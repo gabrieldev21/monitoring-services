e3427ff7c038ddc9533f350a40112ae58434ee64
Gabriel Amorim <gabriel.amorim@profarma.com.br>
2025-09-18 08:06:59 -0300

    fix: retirando metrics e prom client

--- apps/api-gateway/src/modules/app.module.ts
+++ apps/api-gateway/src/modules/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MetricModule } from './metric/metric.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 import { CatalogModule } from './catalog/catalog.module';
 import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [
-    MetricModule,
-    AuthModule,
-    OrderModule,
-    CatalogModule,
-    NotificationModule,
-  ],
+  imports: [AuthModule, OrderModule, CatalogModule, NotificationModule],
 })
 export class AppModule {}
--- apps/api-gateway/src/modules/metric/metric.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MetricService } from './metric.service';
-import { MetricController } from './metric.controller';
-
-@Module({
-  controllers: [MetricController],
-  providers: [MetricService],
-})
-export class MetricModule {}
