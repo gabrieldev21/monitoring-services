165c1950bbc448791deb3b52d0ad8886ad8756df
Gabriel Amorim <gabriel.amorim@profarma.com.br>
2025-09-18 11:45:12 -0300

    fix: ajuste auth controller

--- apps/@shared/infra/otel-sdk.ts
+++ apps/@shared/infra/otel-sdk.ts
@@ -5,7 +5,12 @@ import { FastifyOtelInstrumentation } from '@fastify/otel';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-grpc';
+import {
+  BatchLogRecordProcessor,
+  LoggerProvider,
+} from '@opentelemetry/sdk-logs';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 import { CompressionAlgorithm } from '@opentelemetry/otlp-exporter-base';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR);
@@ -14,16 +19,30 @@ const options = {
   url: 'http://opentelemetry-collector:4317',
   compression: CompressionAlgorithm.GZIP,
 };
+// const options = {
+//   url: 'grpc://opentelemetry-collector:4317',
+//   compression: CompressionAlgorithm.GZIP,
+// };
 const metricExporter = new OTLPMetricExporter(options);
-
-const traceExporter = new OTLPTraceExporter(options);
-
 const metricReader = new PeriodicExportingMetricReader({
   exporter: metricExporter,
-  // exporter: new ConsoleMetricExporter(),
   exportIntervalMillis: Number(5000),
   exportTimeoutMillis: Number(5000),
 });
+
+const traceExporter = new OTLPTraceExporter(options);
+
+const logExporter = new OTLPLogExporter(options);
+const loggerProvider = new LoggerProvider({
+  resource: resourceFromAttributes({
+    'service.name': process.env.SERVICE_NAME ?? 'unknown_service',
+  }),
+  processors: [new BatchLogRecordProcessor(logExporter)],
+  forceFlushTimeoutMillis: 3000,
+});
+
+const logger = loggerProvider.getLogger('app-logger');
+logger.emit({ body: 'Hello OTEL logs!' });
 
 const fastifyOtelInstrumentation = new FastifyOtelInstrumentation({
   registerOnInitialization: true,
