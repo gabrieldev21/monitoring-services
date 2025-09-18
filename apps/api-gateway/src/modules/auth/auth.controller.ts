49bca82de238b573d944c3ba119d5847142a25d6
Gabriel Amorim <gabriel.amorim@profarma.com.br>
2025-09-18 09:00:56 -0300

    fix: ajuste controller

--- apps/api-gateway/src/modules/auth/auth.controller.ts
+++ apps/api-gateway/src/modules/auth/auth.controller.ts
@@ -1,14 +1,9 @@
-import { Body, Controller, Get, Inject, Post, Res } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Post } from '@nestjs/common';
 import axios from 'axios';
 import { Public } from './jwt.util';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
-  ) {}
-
   private msAuthUrl = process.env.MS_AUTH_URL || 'http://ms-auth:3011/auth';
 
   @Public()
